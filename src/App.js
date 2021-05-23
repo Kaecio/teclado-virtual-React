5ba47bb2785749229dec545b838c3c66a7232dd9
Kaecio <56613892+Kaecio@users.noreply.github.com>
2021-05-22 23:54:48 -0300

    Desenvolvendo o teclado virtual em React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  //   const [ligar, setLigar] = useState(false);
+
+  //  function ligaTeclado(){
+
+  //  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <div className="teclado">
+        <div className="header_teclado">
+          <div className="caixa_esq caixa_esquerda"></div>
+          <div className="botoes_esq btn_painel_esquerdo">
+            <div className="caixa1_btns_esq">
+              <div className="circles">
+                <div className="circle_power"></div>
+                <button type="button" className="circle_esq1">
+                  ON
+                </button>
+              </div>
+              <div className="caixa_dos_botoes1">
+                <div className="btns-caixa1">
+                  <div className="btn1 btn"></div>
+                  <div className="btn2 btn"></div>
+                  <div className="btn3 btn"></div>
+                  <div className="btn4 btn"></div>
+                </div>
+                <div className="btns-caixa1_1">
+                  <div className="btn5 btn"></div>
+                  <div className="btn6 btn"></div>
+                  <div className="btn7 btn"></div>
+                  <div className="btn8 btn"></div>
+                </div>
+              </div>
+            </div>
+            <div className="caixa2_btns_esq">
+              <div className="circle_esq2"></div>
+              <div className="btns-caixa2">
+                <div className="btn1" btn></div>
+                <div className="btn2" btn></div>
+                <div className="btn3" btn></div>
+                <div className="btn4" btn></div>
+              </div>
+            </div>
+          </div>
+          <div className="painel">
+            <div className="logo_teclado">YAMAHEI</div>
+            <div className="painel_digital"></div>
+          </div>
+          <div className="botoes_direita btn_painel"></div>
+          <div className="caixa_direita caixa"></div>
+        </div>
+      </div>
+      <div className="teclado_teclas">
+        <div className="oitava" o2>
+          <div className="branca c2" c2></div>
+          <div className="preta cs2" cs2></div>
+          <div className="branca d2" d2></div>
+          <div className="preta ds2" ds2></div>
+          <div className="branca e2" e2></div>
+          <div className="branca f2" f2></div>
+          <div className="preta fs2" fs2></div>
+          <div className="branca g2" g2></div>
+          <div className="preta gs2" gs2></div>
+          <div className="branca a2" a2></div>
+          <div className="preta as2" as2></div>
+          <div className="branca b2" b2></div>
+        </div>
+        <div className="oitava" o3>
+          <div className="branca c3" c3></div>
+          <div className="preta cs3" cs3></div>
+          <div className="branca d3" d3></div>
+          <div className="preta ds3" ds3></div>
+          <div className="branca e3" e3></div>
+          <div className="branca f3" f3></div>
+          <div className="preta fs3" fs3></div>
+          <div className="branca g3" g3></div>
+          <div className="preta gs3" gs3></div>
+          <div className="branca a3" a3></div>
+          <div className="preta as3" as3></div>
+          <div className="branca b3" b3></div>
+        </div>
+        <div className="oitava" o4>
+          <div className="branca c4" c4></div>
+          <div className="preta cs4" cs4></div>
+          <div className="branca d4" d4></div>
+          <div className="preta ds4" ds4></div>
+          <div className="branca e4" e4></div>
+          <div className="branca f4" f4></div>
+          <div className="preta fs4" fs4></div>
+          <div className="branca g4" g4></div>
+          <div className="preta gs4" gs4></div>
+          <div className="branca a4" a4></div>
+          <div className="preta as4" as4></div>
+          <div className="branca b4" b4></div>
+        </div>
+        <div className="oitava" o5>
+          <div className="branca c5" c5></div>
+          <div className="preta cs5" cs5></div>
+          <div className="branca d5" d5></div>
+          <div className="preta ds5" ds5></div>
+          <div className="branca e5" e5></div>
+          <div className="branca f5" f5></div>
+          <div className="preta fs5" fs5></div>
+          <div className="branca g5" g5></div>
+          <div className="preta gs5" gs5></div>
+          <div className="branca a5" a5></div>
+          <div className="preta as5" as5></div>
+          <div className="branca b5" b5></div>
+        </div>
+        <div className="oitava" o6>
+          <div className="branca c6" c6></div>
+          <div className="preta cs6" cs6></div>
+          <div className="branca d6" d6></div>
+          <div className="preta ds6" ds6></div>
+          <div className="branca e6" e6></div>
+          <div className="branca f6" f6></div>
+          <div className="preta fs6" fs6></div>
+          <div className="branca g6" g6></div>
+          <div className="preta gs6" gs6></div>
+          <div className="branca a6" a6></div>
+          <div className="preta as6" as6></div>
+          <div className="branca b6" b6></div>
+        </div>
+        <div className="oitava" o7>
+          <div className="branca" c7></div>
+        </div>
+        <div className="teste"></div>
+      </div>
     </div>
   );
 }
